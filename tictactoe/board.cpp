4b83879381fc26a0c728837e966f5cd0e888eccc
Rita <rita.aksenfeld@gmail.com>
2017-01-14 11:26:53 -0800

    edited Board class

--- tictactoe/board.cpp
+++ tictactoe/board.cpp
@@ -5,31 +5,53 @@
  */
 Board::Board()
 {
-	//
+	// Creates an array of 3 char arrays with 3 elts 
+	//    'U' (for unoccupied) each 
+	char squares[3][3] = {{'U', 'U', 'U'}, {'U', 'U', 'U'}, {'U', 'U', 'U'}};
 }
 
 /**
- * @brief
+ * @brief Given board coordinates, return a square state (U, X, or O)
+ * 
+ * @param col the column coordinate for the board (0 = first column)
+ * @param row the row coordinate for the board (0 = first row)
+ * 
+ * @return the piece at the specifiec coordinate (or 'U' if unoccupied)
  */
 char Board::Query(int col, int row)
 {
-	//
+	return squares[col][row];
 }
 
 /**
- * @brief
+ * @brief Places a game piece on the board at the specified coordinates
+ * 
+ * @param col the column coordinate for the board (0 = first column)
+ * @param row the row coordinate for the board (0 = first row)
+ * @param piece the piece ('X' or 'O') to be placed
  */
 void Board::Place(int col, int row, char piece)
 {
-	//
+	squares[col][row] = piece;
 }
 
 /**
- * @brief
+ * @brief Returns which player won, if any
+ * 
+ * @return the player who won ('X' or 'O') or 'U' if no wins
  */
 char Board::CheckWin()
 {
-	//
+	// Call helper functions
+	char wins = {CheckWinCol(), CheckWinRow(), CheckWinDiag()};
+	for (int i = 0; i < 3; i++)
+	{
+		if (wins[i] == 'X' || wins[i] == 'O')
+		{
+			return wins[i];
+		}
+	}
+	return 'U';
 }
 
 

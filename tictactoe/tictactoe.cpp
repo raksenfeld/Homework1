af4a99892ba3878a6ea4ff5df38d444a2916fdc1
Rita <rita.aksenfeld@gmail.com>
2017-01-14 14:22:25 -0800

    tictactoe

---
+++ tictactoe/tictactoe.cpp
@@ -0,0 +1,43 @@
+/****
+ * *
+* @file tictactoe.cpp
+* @author Rita Aksenfeld <raksenfe@caltech.edu>
+* @date 2017
+*
+* @brief Allows the user(s) to play a Tic-Tac-Toe game
+* 
+* This program prints to standard output and uses user input.
+*
+******/
+
+
+#include <iostream>
+#include <game.hpp>
+#include <board.hpp>
+
+using namespace std;
+
+/**
+ * @brief Creates an instance of the Game class to play Tic-Tac-Toe
+ */
+int main(int argc, char ** argv)
+{
+	while (true)
+	{
+		// Starts a new game
+		Game game;
+		game.Run();
+		
+		// Asks player if want another game
+		char choice;
+		cout << "Do you want to play again? (Y/N)" << endl;
+		cin >> i;
+		
+		// If no, breaks out of loop
+		if (choice == 'N')
+		{
+			break;
+		}
+	}
+    return 0;
+}
